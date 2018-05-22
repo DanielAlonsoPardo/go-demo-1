9f07d0607b56da7991a63aa69c8920ffe7371133
Daniel Alonso Pardo <danielalonsopardo@agaveengineering.com>
2018-05-22 13:05:17 -0600

    Open file, handle file errors

--- main.go
+++ main.go
@@ -12,5 +12,14 @@ func main() {
 		fmt.Println(os.Args[0], "MP3FILE")
 		return
 	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil { fmt.Println(err); return }
+
+	
+	
+	err = file.Close()
+	if err != nil {	fmt.Println(err) }
+	
 	fmt.Println(os.Args)
 }
