119a102866abb8cf2ae366950dfc8ce1f6d68135
Daniel Alonso Pardo <danielalonsopardo@agaveengineering.com>
2018-05-22 17:16:56 -0600

    Argument is now read as an .mp3 and played over the speakers

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
 )
 
 func main() {
@@ -13,13 +17,26 @@ func main() {
 		return
 	}
 
+
+	//Open file
 	file, err := os.Open(os.Args[1])
 	if err != nil { fmt.Println(err); return }
+	//Close file
+	defer func() {
+		err = file.Close()
+		if err != nil {	fmt.Println(err) }
+	}()
 
-	
-	
-	err = file.Close()
-	if err != nil {	fmt.Println(err) }
-	
+	//Decode
+	streamer, format, err := mp3.Decode(file)
+	if err != nil { fmt.Println(err); return }
+
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	done := make(chan struct{})
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {close(done)})))
+
 	fmt.Println(os.Args)
+
+	<-done
 }
