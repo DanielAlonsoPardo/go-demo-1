f4b07480d9fe459220b37fa85acb706fabd0094b
Daniel Alonso Pardo <danielalonsopardo@agaveengineering.com>
2018-05-22 17:47:17 -0600

    Program now only accepts files named '*.mp3'

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"time"
+	"regexp"
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/speaker"
@@ -15,17 +16,26 @@ func main() {
 		fmt.Println("Use: ")
 		fmt.Println(os.Args[0], "MP3FILE")
 		return
-	}
+	} 
 
+	mp3filename, err := regexp.Compile(".*\\.mp3")
+	x := mp3filename.MatchString(os.Args[1])
+	if !x {
+		fmt.Println(os.Args[1])
+		fmt.Println("This program only plays .mp3 files")
+		return
+	}
 
 	//Open file
 	file, err := os.Open(os.Args[1])
 	if err != nil { fmt.Println(err); return }
+	
 	//Close file
 	defer func() {
 		err = file.Close()
 		if err != nil {	fmt.Println(err) }
 	}()
+
 
 	//Decode
 	streamer, format, err := mp3.Decode(file)
